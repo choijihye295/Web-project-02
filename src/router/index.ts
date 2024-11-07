e3927ea8388026479d54415c2df99022380087a8
choijihye <choijihye295@jbnu.ac.kr>
2024-11-07 23:40:12 +0900

    lon in page : SignIn.vue create

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignIn from '../views/sign-in/SignIn.vue' // SignIn.vue 파일을 불러옵니다
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/sign-in', // 로그인 페이지 경로 추가
+    name: 'sign-in',
+    component: SignIn
   }
 ]
 
